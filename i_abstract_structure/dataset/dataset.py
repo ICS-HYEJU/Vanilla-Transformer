67d300bac543d1a25dcaa120863657b85f35636f
hjchoi <juhye987@naver.com>
2024-04-04 23:50:13 +0900

    [ADD] dataset.py

--- i_abstract_structure/dataset/dataset.py
+++ i_abstract_structure/dataset/dataset.py
@@ -1,55 +1,59 @@
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import numpy as np
+import torch
 import os
 import pandas as pd
 import sentencepiece as spm
 from sklearn.model_selection import train_test_split
+import glob
 
 
 class bible_dataset(Dataset):
-    def __init__(self, cfg, seq_len):
+    def __init__(self, cfg, seq_len, mode='train'):
         super().__init__()
         #
         self.cfg = cfg
-        self.data = self.dataset_to_pandas(cfg)
+        self.data = self.dataset_to_pandas()
         self.seq_len = seq_len
         #
         self.sp_src, self.sp_trg = self.build_subtokenizer()
-        self.src_data = self.data['en']
-        self.trg_data = self.data['ko']
 
-        assert self.src_data == True or self.trg_data == True, f'Invalid task...'
+        assert self.data is not None, f'Invalid task...'
         #
-        self.src_list = []
-        self.trg_list = []
-        #
+        if mode == 'train':
+            self.src = self.data['en'][:int(len(self.data) * 0.7)]
+            self.trg = self.data['ko'][:int(len(self.data) * 0.7)]
+        else:
+            self.src = self.data['en'][int(len(self.data) * 0.7):]
+            self.trg = self.data['ko'][int(len(self.data) * 0.7):]
 
     def __len__(self):
-        return len(self.data)
+        return len(self.src)
 
     def __getitem__(self, idx):
-        self.src_list.append(self.en_encode(self.src_data[idx]))
-        self.trg_list.append(self.ko_encode(self.trg_data[idx]))
+        src = self.en_encode(self.src[idx])  # Encode Input
+        trg_input = self.ko_encode(self.trg[idx])  # Decoder Input
+        trg_output = trg_input[1:self.seq_len]  # Decoder Label
+        trg_output = np.pad(trg_output, (0, 1), 'constant', constant_values=0)
 
-        return self.src_list, self.trg_list
+        return torch.Tensor(src).long(), torch.Tensor(trg_input).long(), torch.Tensor(trg_output).long()
 
-
-    def dataset_to_pandas(self, cfg_dataset):
-        if cfg_dataset['name'] == 'bible':
+    def dataset_to_pandas(self):
+        if self.cfg['dataset_info']['name'] == 'bible':
             pass
         else:
             raise ValueError('Invalid dataset name, currently supported [ bible ]')
         #
-        data_path = cfg_dataset['path']
+        data_path = self.cfg['dataset_info']['path']
 
         # load en dataset and read
-        en_train = open(os.path.join(data_path, 'bible-all.en.txt'))
+        en_train = open(os.path.join(data_path, 'bible_all_en.txt'))
         en_train_content = en_train.read()
         en_train_list = en_train_content.split('\n')
 
         # load ko dataset and read
-        ko_train = open(os.path.join(data_path, 'bible-all.ko.txt'))
+        ko_train = open(os.path.join(data_path, 'bible_all_kr.txt'))
         ko_train_content = ko_train.read()
         ko_train_list = ko_train_content.split('\n')
 
@@ -57,7 +61,7 @@ class bible_dataset(Dataset):
         data = pd.DataFrame()
         data['en_raw'] = en_train_list
         data['ko_raw'] = ko_train_list
-        data = data.set_index(drop=True)
+        data = data.reset_index(drop=True)
 
         # Remove the words such as 'Genesis#.#'
         data['en'] = data['en_raw'].apply(lambda x: x.split(' ')[1:])
@@ -103,18 +107,21 @@ class bible_dataset(Dataset):
 
         return tmpstr
 
-    def split_dataset(self):
-        src_train, src_valid, trg_train, trg_valid = train_test_split(self.src_list, self.trg_list, test_size=0.2,
-                                                                      random_state=42)
-        return src_train, src_valid, trg_train, trg_valid
 
 if __name__ == '__main__':
     from i_abstract_structure.config.config import get_config_dict
+
     cfg = get_config_dict()
     #
-    bible = bible_dataset(cfg=cfg, seq_len = cfg['dataset_info']['seq_len'])
+    bible = bible_dataset(cfg=cfg, seq_len=cfg['dataset_info']['seq_len'])
     #
-    src_train, src_valid, trg_train, trg_valid = bible.split_dataset()
+    bible.__getitem__(10)
+    bible_dataloader = torch.utils.data.DataLoader(bible, batch_size=cfg['dataset_info']['batch_size'])
     #
-    loader =DataLoader(bible, batch_size=cfg['dataset_info']['batch_size'])
+    for i, data in enumerate(bible_dataloader):
+        print(data)
+
+
+
+
 
