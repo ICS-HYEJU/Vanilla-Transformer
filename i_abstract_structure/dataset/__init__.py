025352611f7027affd33916cb428b413bb9feb1a
hjchoi <juhye987@naver.com>
2024-04-12 14:52:24 +0900

    [UPDATE] create_dataloader.py - remove) object->path - change) config['dataset_info']['']-> config['']

--- i_abstract_structure/dataset/__init__.py
+++ i_abstract_structure/dataset/__init__.py
@@ -2,37 +2,34 @@ import torch
 from torch.utils.data.dataloader import DataLoader
 
 def create_dataloader(config):
-    if config['dataset_info']['name'] == 'bible':
-        from i_abstract_structure.dataset.dataset import bible as dataset_class
+    if config['name'] == 'bible':
+        from i_abstract_structure.dataset.dataset import bible_dataset as dataset_class
     else:
         raise ValueError('Invalid dataset name, currently supported [ bible ]')
     #
-    data_path = config['dataset_info']['path']
+    data_path = config['path']
     #
     train_object = dataset_class(
-        path = data_path,
         cfg = config,
-        seq_len = config['dataset_info']['seq_len'],
+        seq_len = config['seq_len'],
         mode = 'train',
     )
     train_loader = DataLoader(
         train_object,
-        batch_size = config['dataset_info']['batch_size'],
+        batch_size = config['batch_size'],
         mode = 'train',
         # shuffle = True
         # num_workers = num_workers
     )
     #
     val_object = dataset_class(
-        path = data_path,
         cfg = config,
-        seq_len = config['dataset_info']['seq_len'],
+        seq_len = config['seq_len'],
         mode = 'val'
     )
     val_loader = DataLoader(
         val_object,
-        batch_size = config['dataset_info']['batch_size'],
+        batch_size = config['batch_size'],
         mode = 'val'
     )
     return train_loader, val_loader
-
