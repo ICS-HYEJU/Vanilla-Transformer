cd67ec4d2963463c1a8ef3f22562ed6fdeeb94b3
hjchoi <juhye987@naver.com>
2024-04-12 20:42:51 +0900

    [UPDATE] engine.py

--- i_abstract_structure/core/engine.py
+++ i_abstract_structure/core/engine.py
@@ -1,43 +1,35 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
-import pandas as pd
-from pprint import pprint
-from nltk.tokenize import word_tokenize as en_tokenizer
-import urllib.request
-import csv
-import numpy as np
-from torch.cuda import amp
+# from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+import torch
 import time
-import copy
-from collections import defaultdict
-from sklearn.metrics import mean_squared_error
-import joblib
-import gc
 import os
-from sklearn.model_selection import train_test_split
-import os
+import numpy as np
+
+#
+FP16 = False
+from torch.cuda import amp
+#
+from i_abstract_structure.dataset import train_dataloader, val_dataloader
 
 
 #
-# from ..utils.events import write_tbimg, write_tbloss, write_tbacc, write_tbPR
-# from ..dataset import create_dataloader
-#
+
 class Trainer():
-    def __init__(self, cfg, device=torch.device("cpu")):
+    def __init__(self, cfg, device=torch.device('cpu')):
         self.cfg = cfg
         self.device = device
 
-        # ===== save path =====
+        # ===== Save Path =====
         self.save_path = self.make_save_path()
+
+        # ===== Tensorboard =====
+        # self.tblogger = SummaryWriter(self.save_path)
 
         # ===== DataLoader =====
         self.train_loader, self.val_loader = self.get_dataloader()
 
-        # ===== subtokenizer =====
-        # self.sp_src, self.sp_trg = self.build_subtokenizer()
+        # ===== Model =====
+        self.model = self.build_model()
 
         # ===== Optimizer =====
         self.optimizer = self.build_optimizer()
@@ -52,38 +44,143 @@ class Trainer():
         self.max_epoch = self.cfg['solver']['max_epoch']
         self.max_stepnum = len(self.train_loader)
 
-    # def build_subtokenizer(self):
-    #     from i_abstract_structure.model.subtokenizer import load_subtokenizer
-    #
-    #     sp_src, sp_trg = load_subtokenizer(corpus_src=self.cfg['subtokenizer']['en_corpus'],
-    #                                        corpus_trg=self.cfg['subtokenizer']['ko_corpus'],
-    #                                        vocab_size=self.cfg['dataset_info']['vocab_size'])
-    #     return sp_src, sp_trg
-
-    def calc_loss(self, logits, labels):
-        return self.criterion(logits, labels.float())
+    def calc_loss(self, logits: torch.tensor, targets: torch.tensor):
+        logits = logits.view(-1, self.cfg['dataset_info']['vocab_size'])
+        targets = targets.view(-1)
+        return self.criterion(logits, targets)
 
     def set_criterion(self):
-        return torch.nn.CrossEntropyLoss(ignore_index=self.cfg['dataset_info']['PAD_IDX']).to(self.device)
+        return torch.nn.CrossEntropyLoss(ignore_index=self.cfg['dataset_info']['PAD_IDX'])
+        # Multi Classification-> CrossEntropy
 
-   #  def build_scheduler(self):
-   #      from solver.fn_scheduler import build_scheduler
-   #      return build_scheduler(self.cfg, self.optimizer)
-   #
-   #  def build_optimizer(self):
-   #      from solver.fn_optimizer import build_optimizer
-   #      optim = build_optimizer(self.cfg, self.model)
-   #      return optim
-   #
-   # def build_model(self):
+    def build_scheduler(self):
+        from i_abstract_structure.solver.fn_scheduler import build_scheduler
+        return build_scheduler(self.cfg, self.optimizer)
 
-   # def get_dataloader(self):
-   #     train_loader, val_loader = create_dataloader(cfg['dataset_info'])
+    def build_optimizer(self):
+        from i_abstract_structure.solver.fn_optimizer import build_optimizer
+        return build_optimizer(self.cfg, self.model)
+
+    def build_model(self):
+        name = self.cfg['model']['name']
+        if name == 'Transformer':
+            from i_abstract_structure.model.Transformer import TransformerModel
+            model = TransformerModel(seq_len=self.cfg['dataset_info']['seq_len'],
+                                     vocab_size=self.cfg['dataset_info']['vocab_size']).to(self.device)
+        else:
+            raise NotImplementedError(f'The required model is not implemented yet...')
+        return model
+
+    def get_dataloader(self):
+        train_loader = train_dataloader(self.cfg, mode='train')
+        val_loader = val_dataloader(self.cfg, mode='val')
+        return train_loader, val_loader
+
+    def make_save_path(self):
+        save_path = os.path.join(self.cfg['path']['save_base_path'],
+                                 self.cfg['model']['name'])
+        os.makedirs(save_path, exist_ok=True)
+        return save_path
+
+    def start_train(self):
+        try:
+            print(f'Training Start..')
+            start_time = time.time()
+            for epoch in range(self.max_epoch):
+                self.train_one_epoch(epoch)
+            print(f'\nTraining completed in {(time.time() - start_time) / 3600:.3f} hours.')
+
+            self.scheduler.step()
+        except Exception as _:
+            print('ERROR in training loop or eval/save model.')
+            raise
+
+    def train_one_epoch(self, epoch):
+        # Set Trian Mode
+        self.model.train()
+
+        if (FP16):
+            scaler = amp.GradScaler()
+
+        dataset_size = 0
+        running_loss = 0
+        running_accuracy = 0
+        accuracy = 0
+
+        bar = tqdm(enumerate(self.train_loader), total=len(self.train_loader))
+
+        for step, data in bar:
+            src = data[0].to(self.device)
+            trg_input = data[1].to(self.device)
+            trg_output = data[2].to(self.device)
+
+            batch_size = src.shape[0]
+
+            if (FP16):
+                with amp.autocast(enabled=True):
+                    output, logits = self.model(enc_src=src, dec_src=trg_input)
+                    loss = self.calc_loss(logits, trg_output)
+
+                    # Scaled loss
+                    # To call Scaled Gradients, backward() scaled loss
+                    scaler.scale(loss).backward()
+                    #  |-> AssertionError : assert outputs.is_cuda or outputs.device.type == 'xla'
+                    # scaler.step() first unscales the gradients of the optimizer's assinged params.
+                    # If these gradients do not contain infs of NaNs, optimizer.step() is then called.
+                    # Otherwise, optimizer.step() is skipped.
+                    scaler.step(self.optimizer)
+
+                    # Updates the scale for next iteration
+                    scaler.update()
+
+            else:
+                output, logits = self.model(enc_src=src, dec_src=trg_input)
+                loss = self.calc_loss(logits, trg_output)
+
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
+                self.optimizer.step()
+
+            # logits.shape = (bs, seq_len, vocab_size)
+            # trg_output.shape = (bs, seq_len)
+
+            # zero the parameter graidents
+            self.optimizer.zero_grad()
+
+            # loss.item() is transformation loss to Python Float
+            # loss.item() is loss of batch data. So, To calculate sum of loss, * (batch_size)
+            running_loss += loss.item() * batch_size
+            running_accuracy = np.mean(
+                output.view(-1).detach().cpu().numpy() == trg_output.view(-1).detach().cpu().numpy
+            )
+
+            accuracy += running_accuracy
+
+            dataset_size += batch_size
+            epoch_loss = running_loss / dataset_size
+
+            bar.set_postfix(
+                Epoch=epoch, Train_loss=epoch_loss, LR=self.optimizer.param_groups[0]["lr"],
+                accuracy=accuracy / float(step + 1)
+            )
+
+            # break
+
+        accuracy /= len(self.train_loader)
+
+        # Change learning rate by Scheduler
+        if self.scheduler is not None:
+            self.scheduler.step()
+
+        return epoch_loss, accuracy
 
 
 if __name__ == '__main__':
     from i_abstract_structure.config.config import get_config_dict
 
+    # Get configuration
     cfg = get_config_dict()
-
+    # Get Traininer
     trainer = Trainer(cfg)
+    # Start train
+    trainer.start_train()
